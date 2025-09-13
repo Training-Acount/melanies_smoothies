8c6720b100acd9bcdbbe2403d2b14961de6634e2
Training-Acount <mfharraz@yahoo.com>
2025-09-12 21:15:12 -0400

    Update streamlit_app.py
    get chosen fruit info

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,9 +35,10 @@ if options:
 
     for fruit_chosen in options:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen)
         # st.text(smoothiefroot_response.json())
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), width=True)
 
     # st.write(ingredients_string)
 
